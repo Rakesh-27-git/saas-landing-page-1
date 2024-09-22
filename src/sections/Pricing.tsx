8c2d3b7071a060b930020ce2175a2cba5efd2dab
Rakesh Choudhary <seervir987@gmail.com>
2024-09-22 13:29:14 +0530

    Add Testimonial, CallToAction and Footer section.

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import CheckIcon from "@/assets/check.svg";
 import { twMerge } from "tailwind-merge";
 import { motion } from "framer-motion";
--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,14 +1,26 @@
+"use client";
 import productImage from "@/assets/product-image.png";
 import pyramidImage from "@/assets/pyramid.png";
 import tubeImage from "@/assets/tube.png";
 import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export const ProductShowcase = () => {
+  const sectionRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
   return (
-    <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip">
+    <section
+      ref={sectionRef}
+      className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip"
+    >
       <div className="container">
         <div className="section-heading">
-          <div className="flex items-center justify-center">
+          <div className="flex justify-center">
             <div className="tag">Boost your productivity</div>
           </div>
           <h2 className="section-title mt-5">
@@ -21,19 +33,25 @@ export const ProductShowcase = () => {
         </div>
         <div className="relative">
           <Image src={productImage} alt="Product Image" className="mt-10" />
-          <Image
-            src={pyramidImage}
+          <motion.img
+            src={pyramidImage.src}
             alt="Pyramid Image"
             height={262}
             width={262}
             className="hidden md:block absolute -right-36 -top-32"
+            style={{
+              translateY,
+            }}
           />
-          <Image
-            src={tubeImage}
-            alt="Tube Image"
+          <motion.img
+            src={tubeImage.src}
+            alt="Tube image"
             height={248}
             width={248}
             className="hidden md:block absolute bottom-24 -left-36"
+            style={{
+              translateY,
+            }}
           />
         </div>
       </div>
